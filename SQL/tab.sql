17e0de4b97b8bfabb5fb6a1fbc4059e25f58cb8f
Kilian Dumaire <Kilian@MacBook-Pro-de-coding-5.local>
2023-10-16 16:29:45 +0200

    correction des clés étrangères

--- SQL/tab.sql
+++ SQL/tab.sql
@@ -14,6 +14,13 @@ CHARACTER SET 'utf8'
 ENGINE = INNODB;
 
 
+CREATE TABLE IF NOT EXISTS jeu (
+    id INT(11)UNSIGNED NOT NULL AUTO_INCREMENT ,
+    nom_jeu VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
+)
+CHARACTER SET 'utf8'
+ENGINE = INNODB;
 
 CREATE TABLE IF NOT EXISTS score (
     id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -22,14 +29,12 @@ CREATE TABLE IF NOT EXISTS score (
     difficulte VARCHAR(15) NOT NULL, 
     score INT(11) NOT NULL, 
     date_heure_partie DATETIME NOT NULL,
-    PRIMARY KEY (id)
-    CONSTRAINT fk_score_utilisateur FOREIGN KEY id_joueur REFERENCES utilisateur(id) ON DELETE SET NULL
-    CONSTRAINT fk_score_jeu FOREIGN KEY id_jeu REFERENCES jeu(id) ON DELETE SET NULL
+    PRIMARY KEY (id),
+    CONSTRAINT fk_score_utilisateur FOREIGN KEY (id_joueur) REFERENCES utilisateur(id) ON DELETE SET NULL,
+    CONSTRAINT fk_score_jeu FOREIGN KEY (id_jeu) REFERENCES jeu(id) ON DELETE SET NULL
 )
 CHARACTER SET 'utf8'
 ENGINE = INNODB;
-
-
 
 CREATE TABLE IF NOT EXISTS messages (
     id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -37,19 +42,9 @@ CREATE TABLE IF NOT EXISTS messages (
     id_expediteur INT(11) UNSIGNED NOT NULL,
     texte_message TEXT NOT NULL,
     date_heure_message DATETIME NOT NULL,
-    PRIMARY KEY(id)
-    CONSTRAINT fk_message_jeu FOREIGN KEY id_jeu REFERENCES jeu(id) ON DELETE SET NULL
-    CONSTRAINT fk_message_utilisateur FOREIGN KEY id_expediteur REFERENCES utilisateur(id) ON DELETE SET NULL
-)
-CHARACTER SET 'utf8'
-ENGINE = INNODB;
-
-
-
-CREATE TABLE IF NOT EXISTS jeu (
-    id INT(11)UNSIGNED NOT NULL AUTO_INCREMENT ,
-    nom_jeu VARCHAR(50) NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    CONSTRAINT fk_message_jeu FOREIGN KEY (id_jeu) REFERENCES jeu(id) ON DELETE SET NULL,
+    CONSTRAINT fk_message_utilisateur FOREIGN KEY (id_expediteur) REFERENCES utilisateur(id) ON DELETE SET NULL
 )
 CHARACTER SET 'utf8'
 ENGINE = INNODB;
