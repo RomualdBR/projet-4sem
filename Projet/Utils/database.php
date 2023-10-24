b57bef3c478d7f2b9e07ce7b64fb7538fe3930b6
Kilian Dumaire <Kilian@MacBook-Pro-de-coding-5.local>
2023-10-24 11:51:39 +0200

    rahout de la fonction connectToDbAndGetPdo

--- Projet/Utils/database.php
+++ Projet/Utils/database.php
@@ -1,19 +1,22 @@
 <?php
-$dbname = 'the_power_of_memory';
-$host = 'localhost';
+function connectToDbAndGetPdo(): void
+{
+    $dbname = 'the_power_of_memory';
+    $host = 'localhost';
 
-$dsn = "mysql:dbname=$dbname;host=$host;charset=utf8";
-$user = 'root';
-$pass = '';
+    $dsn = "mysql:dbname=$dbname;host=$host;charset=utf8";
+    $user = 'root';
+    $pass = 'root';
 
-$driver_options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
-];
+    $driver_options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+    ];
 
-try {
-    $pdo = new PDO($dsn, $user, $pass, $driver_options);
-    return $pdo;
-} catch (PDOException $e) {
-    echo 'La connexion à la base de données a échouée.';
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $driver_options);
+        return $pdo;
+    } catch (PDOException $e) {
+        echo 'La connexion à la base de données a échouée.';
+    }
 }
