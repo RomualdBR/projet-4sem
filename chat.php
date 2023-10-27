4baae20474d2d9b81069374d7d04489425df273f
Kilian Dumaire <Kilian@mbp-de-coding-5.home>
2023-10-27 05:38:20 +0200

    finition chat

--- chat.php
+++ chat.php
@@ -7,6 +7,16 @@
     <?php require_once SITE_ROOT . "Projet/Partials/Head.php" ?>
 
 </head>
+<?php
+$pdo = connectToDbAndGetPdo();
+$pdoStatement = $pdo->prepare('SELECT M.texte_message as texte_message, M.id_expediteur as id_expediteur, U.pseudo as pseudo, M.date_heure_message as date_heure_message
+FROM messages AS M
+INNER JOIN utilisateur AS U
+ON M.id_expediteur = U.id
+WHERE M.date_heure_message >= NOW() - INTERVAL 1 DAY;');
+$pdoStatement->execute([]);
+$GlobalMessage = $pdoStatement->fetchAll();
+?>
 
 <body>
 
@@ -16,54 +26,46 @@
             <p>Chat Générale</p>
         </div>
 
+        
         <div class="tchat-message">
-            <div class="tchat-usermessage">
-                <p class="tchat-usermessageinfo autor">Moi</p>
-                <p class="tchat-usermessageconteiner">Eh mec ! J'ai depasser ton record !</p>
-                <p class="tchat-usermessageinfo">Aujourd'hui à 17:32</p>
+                    <?php foreach ($GlobalMessage as $Messages) : ?>
+                        <?php if ($_SESSION["userId"] == $Messages->id_expediteur) : ?>
+                            <div class="tchat-usermessage">
+                                <p class="tchat-usermessageinfo autor"><?php echo $Messages->pseudo ?></p>
+                                <p class="tchat-usermessageconteiner"><?php echo $Messages->texte_message ?></p>
+                                <p class="tchat-usermessageinfo"><?php echo $Messages->date_heure_message ?></p>
+                            </div>
+                        <?php else : ?>
+                            <div class="tchat-othermessage">
+                                <img src="<?= PROJECT_FOLDER ?>asset/images/gégé.png" alt="other" class="tchat-otherphotoprofile">
+                                <p class="tchat-othermessageinfo"><?php echo $Messages->pseudo ?></p>
+                                <p class="tchat-othermessageconteiner"><?php echo $Messages->texte_message ?></p>
+                                <p class="tchat-othermessageinfo"><?php echo $Messages->date_heure_message ?></p>
+                            </div>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+
+
+            <div class="tchat-textmessage">
+                <form method="post">
+                    <input type="text" name="message" placeholder="Ecrire..." class="tchat-tchatbox" required>
+                    <input type="submit" name="envoyer" class="tchat-submit">
+                </form>
             </div>
-
-            <div class="tchat-othermessage">
-                <img src="<?= PROJECT_FOLDER ?>asset/images/gégé.png" alt="other" class="tchat-otherphotoprofile">
-                <p class="tchat-othermessageinfo">other</p>
-                <p class="tchat-othermessageconteiner">Ah bon ? Pourtant j'avais fais
-                    de mon mieux pour qu'il sois imbattable !</p>
-                <p class="tchat-othermessageinfo">Aujourd'hui à 17:34</p>
-            </div>
-
-            <div class="tchat-usermessage">
-                <p class="tchat-usermessageinfo autor">Moi</p>
-                <p class="tchat-usermessageconteiner">Je suis juste meilleur que toi</p>
-                <p class="tchat-usermessageinfo">Aujourd'hui à 17:36</p>
-            </div>
-
-            <div class="tchat-usermessage">
-                <p class="tchat-usermessageinfo autor">Moi</p>
-                <p class="tchat-usermessageconteiner">Tu peux toujours essayer de me battre</p>
-                <p class="tchat-usermessageinfo">Aujourd'hui à 17:36</p>
-            </div>
-
-            <div class="tchat-othermessage">
-                <img src="<?= PROJECT_FOLDER ?>asset/images/gégé.png" alt="other" class="tchat-otherphotoprofile">
-                <p class="tchat-othermessageinfo">other</p>
-                <p class="tchat-othermessageconteiner">Je vais Réussir ! Tu vas voir !</p>
-                <p class="tchat-othermessageinfo">Aujourd'hui à 17:37</p>
-            </div>
-
-            <div class="tchat-othermessage">
-                <img src="<?= PROJECT_FOLDER ?>asset/images/gégé.png" alt="other" class="tchat-otherphotoprofile">
-                <p class="tchat-othermessageinfo">other</p>
-                <p class="tchat-othermessageconteiner">Mais ça risque d'être plus compliquer que prevue</p>
-                <p class="tchat-othermessageinfo">Aujourd'hui à 17:39</p>
-            </div>
-        </div>
-
-
-        <div class="tchat-textmessage">
-            <input type="text" name="message" placeholder="Ecrire..." class="tchat-tchatbox" required>
-            <input type="submit" name="envoyer" class="tchat-submit">
         </div>
     </section>
+    <?php
+    if (isset($_POST["message"])) {
+        $pdo = connectToDbAndGetPdo();
+        $pdoStatement = $pdo->prepare('INSERT INTO messages(id_jeu, id_expediteur, texte_message, date_heure_message)
+                VALUES  (1, :id_expediteur, :text_message, NOW());');
+        $pdoStatement->execute([
+            ':text_message' => $_POST["message"],
+            ":id_expediteur" => $_SESSION["userId"]
+        ]);
+        $userModif = $pdoStatement->fetch();
+    }
+    ?>
 </body>
 
 </html>
