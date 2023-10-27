d36006def528d46545d0c52525a165c8ad692745
Kilian Dumaire <Kilian@mbp-de-coding-5.home>
2023-10-27 03:02:52 +0200

    modification possible Email MDP

--- MyAccount.php
+++ MyAccount.php
@@ -17,7 +17,7 @@ require_once "Projet/Utils/common.php";
     <!--Main-->
     <?php
     $pdo = connectToDbAndGetPdo();
-    $pdoStatement = $pdo->prepare('SELECT email, mot_de_passe FROM utilisateur WHERE id = :id');
+    $pdoStatement = $pdo->prepare('SELECT id, email, mot_de_passe, pseudo FROM utilisateur WHERE id = :id');
     $pdoStatement->execute([
         ":id" => $_SESSION["userId"]
     ]);
@@ -25,39 +25,128 @@ require_once "Projet/Utils/common.php";
     ?>
 
     <?php
+    //Modification de l'adresse mail
+
     if (isset($_POST['nouveau_Email'])) {
         $verifEmail = verifEmail($_POST['nouveau_Email']);
-        $verifEmailacc = verifEmailacc($POST['nouveau_Email']);
+        $verifEmailacc = verifEmailacc($_POST['nouveau_Email']);
 
         if (!$verifEmail) {
             $textEmail = "L'entrée n'est pas une adresse Email";
-        }
-        elseif (!$verifEmailacc){
+        } elseif ($verifEmailacc) {
             $textEmail = "L'adresse mail n'est pas disponible";
-        }
-        else{
+        } else {
             $textEmail = "";
         }
-    }
-    else{
+    } else {
         $textEmail = "";
     }
 
     if (isset($_POST['nouveau_CEmail'])) {
-        $verifNCEmail = verifEmail($_POST['nouveau_CEmail']);
+        $verifNCEmail = $_POST['nouveau_CEmail'] == $_POST['nouveau_Email'];
 
         if (!$verifNCEmail) {
-            $textNCEmail = "L'entrée n'est pas une adresse mail";
-        }
-        else{
+            $textNCEmail = "L'entrée est différente de la nouvelle adresse mail";
+        } else {
             $textNCEmail = "";
         }
+    } else {
+        $textNCEmail = "";
     }
-    else{
+
+    if (isset($_POST['confirmation_Email'])) {
+        $verifCMDP = $userModif->mot_de_passe == password_hash($_POST['confirmation_Email'], PASSWORD_DEFAULT);
+
+        if (!$verifCMDP) {
+            $textCEmail = "Mot de passe incorecte";
+        } else {
+            $textCEmail = "";
+        }
+    } else {
         $textCEmail = "";
     }
 
+    if (isset($_POST['nouveau_Email']) && isset($_POST['nouveau_CEmail']) && isset($_POST['confirmation_Email'])) {
+        if ($verifEmail && !$verifEmailacc && $verifNCEmail && $verifCMDP) {
+            $pdo = connectToDbAndGetPdo();
+            $pdoStatement = $pdo->prepare('UPDATE utilisateur
+                    SET email = :Email
+                    WHERE id = :id');
+            $pdoStatement->execute([
+                ':Email' => $_POST['nouveau_Email'],
+                ':id' => $userModif->id
+            ]);
+
+            $textRéaEmail = "Travaille terminé !";
+            header('location: index.php');
+        } else {
+            $textRéaEmail = "";
+        }
+    } else {
+        $textRéaEmail = "";
+    }
+
+
+
+    //Modification de mot de passe
+
+    if (isset($_POST['AncienMDP'])) {
+        $verifAncienMDP = $userModif->mot_de_passe == password_hash($_POST['AncienMDP'], PASSWORD_DEFAULT);
+
+        if (!$verifAncienMDP) {
+            $textAncienMDP = "Mot de passe incorecte";
+        } else {
+            $textAncienMDP = "";
+        }
+    } else {
+        $textAncienMDP = "";
+    }
+
+    if (isset($_POST['NouveauMDP'])) {
+        $verifNouveauMDP = verifMdp($_POST['NouveauMDP']);
+
+        if (!$verifNouveauMDP) {
+            $textNouveauMDP = "Ne correspond pas au critères";
+        } else {
+            $textNouveauMDP = "";
+        }
+    } else {
+        $textNouveauMDP = "";
+    }
+
+    if (isset($_POST['ConfirmMDP'])) {
+        $verifConfirmMDP = $_POST['NouveauMDP'] == $_POST['ConfirmMDP'];
+
+        if (!$verifConfirmMDP) {
+            $textConfirmMDP = "les mots de passe ne correspondent pas";
+        } else {
+            $textConfirmMDP = "";
+        }
+    } else {
+        $textConfirmMDP = "";
+    }
+
+    if (isset($_POST['AncienMDP']) && isset($_POST['NouveauMDP']) && isset($_POST['ConfirmMDP'])) {
+        if ($verifAncienMDP && $verifNouveauMDP && $verifConfirmMDP) {
+            $pdo = connectToDbAndGetPdo();
+            $pdoStatement = $pdo->prepare('UPDATE utilisateur
+                    SET mot_de_passe = :mot_de_passe
+                    WHERE id = :id');
+            $pdoStatement->execute([
+                ':mot_de_passe' => password_hash($_POST['NouveauMDP'], PASSWORD_DEFAULT),
+                ':id' => $userModif->id
+            ]);
+
+            $textRéaMDp = "Travaille terminé !";
+        } else {
+            $textRéaMDp = "";
+        }
+    } else {
+        $textRéaMDp = "";
+    }
     ?>
+
+
     <section class="MyAccount">
         <section class="Banière_profil_utilisateur">
             <div class="bloc_utilisateur">
@@ -66,9 +155,9 @@ require_once "Projet/Utils/common.php";
             </div>
             <div class="caracteristique_utilisateur">
                 <h3 class="info_utilisateur">Information utilisateur :</h3>
-                <p class="info_utilisateur1"> <span class="bold">Nom, prenom : </span>BRISACIER Romuald</p>
+                <p class="info_utilisateur1"> <span class="bold">Pseudo : </span><?php echo $userModif->pseudo ?></p>
                 <p class="info_utilisateur2"> <span class="bold">Date de naissace : </span>JJ/MM/AA</p>
-                <p class="info_utilisateur3"> <span class="bold">Email : </span>monemail@gmail.com</p>
+                <p class="info_utilisateur3"> <span class="bold">Email : </span><?php echo $userModif->email ?></p>
                 <p class="info_utilisateur4"><span class="bold">score : </span> 18 363</p>
             </div>
             <div class="description_utilisateur">
@@ -78,48 +167,65 @@ require_once "Projet/Utils/common.php";
         </section>
 
         <section class="changement_info_utilisateur">
-            <form action="changement_info_mail" method="post">
+            <form method="post">
                 <h4 class="changement_info_mail4">Veuillez suivre les étapes suivantes si vous désirez changer de mots de passe :</h4>
                 <p class="changement_info_mail0">Entrez votre email actuelle :</p>
-                <input value="<?php echo $userModif->email?>" type="text" name="ancien_Email" class="changement_info_mail2" required="required" placeholder="mail">
 
-                <p class="changement_info_mail1">entrez votre nouvel email :</p>
+                <input value="<?php echo $userModif->email ?>" type="text" name="ancien_Email" class="changement_info_mail2" required="required" placeholder="mail">
+
+                <p class="changement_info_mail1">entrez votre nouvelle email :</p>
                 <input type="text" name="nouveau_Email" class="changement_info_mail2" required="required" placeholder="mail">
                 <br>
 
-                <?php echo $textEmail ?>
+                <p class="changement_info_mail1"><?php echo $textEmail ?></p>
 
                 <br>
                 <p class="changement_info_mail1">Confirmer votre nouveau mail :</p>
                 <input type="text" name="nouveau_CEmail" class="changement_info_mail2" required="required" placeholder="mail">
                 <br>
 
-                <?php echo $textNCEmail ?>
+                <p class="changement_info_mail1"><?php echo $textNCEmail ?></p>
 
                 <br>
                 <p class="changement_info_mail1">Entrer votre mot de passe :</p>
                 <input type="password" name="confirmation_Email" class="changement_info_mail2" required="required" placeholder="mot de passe">
                 <br>
 
-                <?php echo $textCEmail ?>
+                <p class="changement_info_mail1"><?php echo $textCEmail ?></p>
 
                 <br>
-                <input type="submit" name="submit" value="Confirmer" id="submit" class="changement_info_mail3">
+                <input type="submit" name="submit" class="changement_info_mail3">
             </form>
 
-            <form action="changement_info_mots_de_passe">
+
+
+            <form method="post">
                 <h4 class="changement_info_mots_de_passe4">Veuillez suivre les étapes suivantes si vous désirez changer de mots de passe :</h4>
                 <p class="changement_info_mots_de_passe0">Entrez votre ancien mot de passe : </p>
-                <input value="<?php echo $userModif->mot_de_passe?>" type="password" class="changement_info_mots_de_passe2" required="required" placeholder="mot de passe">
+                <input type="password" name="AncienMDP" class="changement_info_mots_de_passe2" required="required" placeholder="mot de passe">
+                <br>
 
-                <p class="changement_info_mots_de_passe1">Entrez votre nouveau mot de passe : </p>
-                <input type="password" name="" id="" class="changement_info_mots_de_passe2" required="required" placeholder="mot de passe">
-
-                <p class="changement_info_mots_de_passe1">Confirmez votre nouveau mot de passe : </p>
-                <input type="password" class="changement_info_mots_de_passe2" required="required" placeholder="mot de passe">
+                <p class="changement_info_mail1"><?php echo $textAncienMDP ?></p>
 
                 <br>
-                <input type="submit" id="submit" value="Confirmer" class="changement_info_mots_de_passe3">
+                <p class="changement_info_mots_de_passe1">Entrez votre nouveau mot de passe : </p>
+                <input type="password" name="NouveauMDP" id="" class="changement_info_mots_de_passe2" required="required" placeholder="mot de passe">
+                <br>
+
+                <p class="changement_info_mail1"><?php echo $textNouveauMDP ?></p>
+
+                <br>
+                <p class="changement_info_mots_de_passe1">Confirmez votre nouveau mot de passe : </p>
+                <input type="password" name="ConfirmMDP" class="changement_info_mots_de_passe2" required="required" placeholder="mot de passe">
+                <br>
+
+                <p class="changement_info_mail1"><?php echo $textConfirmMDP ?></p>
+
+                <br>
+                <input type="submit" class="changement_info_mots_de_passe3">
+                <br>
+                <p class="changement_info_mail1"><?php echo $textRéaMDp ?></p>
+
             </form>
         </section>
     </section>
