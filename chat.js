b14712774e22207bbe674ecfa9b7893e1765a39d
Kilian Dumaire <Kilian@mbp-de-coding.home>
2023-11-03 02:35:04 +0100

    réalisation du chat en JS

--- chat.js
+++ chat.js
@@ -27,11 +27,35 @@ document.getElementById('form_message').addEventListener('submit', function(e) {
 
   function displayTchat(message){
 
-    var repèreTchat = document.getElementById("tchat-textmessage")
-    var creationTchat = document.createElement("div")
-    creationTchat.class = "tchat-message"
-    creationTchat.id = "tchat-message"
-    repèreTchat.insertAdjacentHTML('afterend', creationTchat)
+    //Erreur : la fonction Date() n'existe pas
+
+    //var Horraire = new Date();
+    //var Year = Horraire.getFullYear();
+    //var Month = Horraire.getMonth();
+    //var Date = Horraire.getDate();
+    //var Hours = Horraire.getHours();
+    //var Minutes = Horraire.getMinutes();
+    //var Seconds = Horraire.getSeconds();
+
+    var creationTchat = document.createElement('div');
+    creationTchat.class = "tchat-usermessage";
+    document.getElementById("tchat-message").appendChild(creationTchat);
+    console.log("Verif Une OKEY")
+
+    var userAutor = document.createElement('p');
+    userAutor.class = "tchat-usermessageinfo autor";
+    userAutor.innerHTML = "<?php echo $userConnecter->pseudo ?>";
+    console.log("Verif Deux OKEY")
+
+    var userConteiner = document.createElement('p');
+    userConteiner.class = "tchat-usermessageconteiner";
+    userConteiner.innerHTML = message;
+    console.log("Verif Trois OKEY")
+
+    //var dateInfo = document.createElement('p');
+    //dateInfo.class = "tchat-usermessageinfo";
+    //dateInfo.innerHTML = Year + "-" + Month + "-" + Date + " " + Hours + ":" + Minutes + ":" + Seconds;
+    //console.log("Verif Quatre OKEY")
 
 
 }
